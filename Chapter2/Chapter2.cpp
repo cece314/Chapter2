75f5a3babf3d389647959fb3943fb9e05430d332
cece314 <clq271@email.vccs.edu>
2024-10-22 19:11:37 -0400

    energy

--- Chapter2/Chapter2.cpp
+++ Chapter2/Chapter2.cpp
@@ -6,4 +6,8 @@ using namespace std;
 
 int main()
 {
+	int citrus, multiple;
+	multiple = 0.15 * 16500;
+	citrus = 0.58 * 16500;
+	cout << "According to the survey: \n" << multiple << " " "customers bought more than one drink per week \n" << citrus << " " "customers preferred citrus-flavored drinks";
 }
